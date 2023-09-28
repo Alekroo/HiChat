023a5f246e1c403914ca5662175b12f63b7fdc4f
A1ekr0 <alekrol97@gmail.com>
2023-09-28 18:40:43 +0200

    Add project files.

---
+++ ChatServer/Program.cs
@@ -0,0 +1,83 @@
+﻿using ChatServer.Net.IO;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+
+        static TcpListener _listener;
+
+        static List<Client> _users;
+
+
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+                BroadcastConnection();
+            }
+
+            /*Broadcast connections to everyone on the server*/
+            
+
+        }
+
+
+        static void BroadcastConnection()
+        {
+            foreach (var user in _users)
+            {
+                foreach(var usr in _users) 
+                {
+                    var broadcastPacket = new PacketBuilder();
+                    broadcastPacket.WriteOpCode(1);
+                    broadcastPacket.WriteMessage(usr.Username);
+                    broadcastPacket.WriteMessage(usr.UID.ToString());
+                    user.clientSocket.Client.Send(broadcastPacket.getPacketBytes());  
+                }
+            }
+        }
+
+        public static void BroadcastMessage(string message)
+        {
+            foreach(var user in _users)
+            {
+                var msgPacket = new PacketBuilder();
+                msgPacket.WriteOpCode(5);
+                msgPacket.WriteMessage(message);
+                user.clientSocket.Client.Send(msgPacket.getPacketBytes());
+            }
+        }
+
+        public static void BroadcastDisconnect(string uid)
+        {
+            var disconnectedUser = _users.Where(x => x.UID.ToString() == uid).FirstOrDefault();
+            _users.Remove(disconnectedUser);
+            foreach (var user in _users)
+            {
+                var broadcastPacket = new PacketBuilder();
+                broadcastPacket.WriteOpCode(10);
+                broadcastPacket.WriteMessage(uid);
+                user.clientSocket.Client.Send(broadcastPacket.getPacketBytes());
+            }
+
+            BroadcastMessage($"{disconnectedUser.Username} disconnected!");
+        }
+
+
+    }
+
+
+
+}
---
+++ MyChat/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MyChat.MVVM.Core;
+using MyChat.MVVM.Model;
+using MyChat.Net;
+
+namespace MyChat.MVVM.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+        public RelayCommand ConnectToServerCommand { get; set; }
+        public RelayCommand SendMessageCommand { get; set; }
+        private Server _server;
+
+        public string Username {  get; set; }
+
+        private string _message;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                if (_message != value)
+                {
+                    _message = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Ipadr { get; set; }
+        public int Port { get; set; }
+
+        public MainViewModel() {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            _server = new Server();
+            _server.connectedEvent += UserConnected;    
+            _server.msgRecievedEvent += MessageRecieved;    
+            _server.userDisconnectEvent += RemoveUser;    
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username, Ipadr, Port), o=> !string.IsNullOrEmpty(Username));
+            SendMessageCommand = new RelayCommand(o => { _server.SendMessageToServer(Message); Message = ""; },
+                o => !string.IsNullOrEmpty(Message)); 
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+
+        private void MessageRecieved()
+        {
+            var message = _server.PacketReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(message));
+        }
+
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                Username = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+            };
+
+            if (!Users.Any(x => x.UID == user.UID))
+            {
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+        }
+
+
+
+
+    }
+}
